491ab01473ad68b768f0f50510309854e20fa56b
Prasanna Shrestha <shresthaprasanna230@gmail.com>
2024-01-07 01:16:08 +0545

    creating netflix card slider

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -8,6 +8,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies, getGenres } from "../store";
+import Slider from "../components/Slider";
 
 const Netflix = () => {
   const navigate = useNavigate();
@@ -58,6 +59,7 @@ const Netflix = () => {
           </div>
         </div>
       </div>
+      <Slider movies={movies} />
     </Container>
   );
 };
